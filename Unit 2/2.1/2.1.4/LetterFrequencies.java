8aead1a2ec982b4fd68b3403013b8110b2d7db78
banana man <129533800+RealGSCGAMING@users.noreply.github.com>
2025-09-23 10:01:33 -0700

    last things for today

---
+++ Unit 2/2.1/2.1.4/LetterFrequencies.java
@@ -0,0 +1,15 @@
+/*
+ * Activity 2.1.4
+ */
+public class LetterFrequencies
+{
+	public static void main(String[] args)
+	{
+		String letters = "abcdefghijklmnopqrstuvwxyz"; 
+		String phrase = "This is a phrase!";
+
+		System.out.println("Letter frequencies in '" + phrase + "'");
+
+    /* your code here */
+	}
+}
