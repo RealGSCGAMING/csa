e697267938d47c266a80c0abb61784ad550d800b
banana man <129533800+RealGSCGAMING@users.noreply.github.com>
2026-04-06 21:45:39 -0700

    updated b side label script

--- Random/BSideLabel.java
+++ Random/BSideLabel.java
@@ -11,62 +11,70 @@ public class BSideLabel {
     public static final boolean TENSO = true;
     public static final boolean TARIFF = true;
 
-    public static final double CONVERSION = 159.69;
+    public static final double CONVERSION = 159.77;
 
     public static int presetStickers = -1;
     public static int presetShirts = -1;
+
+    public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
 
         clearConsole();
 
-        Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to the B SIDE LABEL price calculator");
 
         while (true) {
             System.out.println();
+
+            // INPUTS
+
             int amountStickers, amountShirts, amountLicensedStickers;
 
-            // Get amount of stickers
-            if (presetStickers == -1) {
-                System.out.println("How many stickers do you want to buy");
-                amountStickers = Integer.parseInt(sc.nextLine());
-                System.out.println("How many of them are licensed");
-                amountLicensedStickers = Integer.parseInt(sc.nextLine());
-            } else {
-                amountStickers = presetStickers;
-                amountLicensedStickers = 0;
-            }
-
-            // Get amount of shirts
-            if (presetShirts == -1) {
-                System.out.println("How many shirts do you want to buy");
-                amountShirts = Integer.parseInt(sc.nextLine());
-            } else {
-                amountShirts = presetShirts;
-            }
-            
-            if (amountStickers <= 0 && amountShirts <= 0) {
+            // All items
+            System.out.println("How many items are in your cart");
+            int totalItems = getIntInput();
+            if (totalItems <= 0) {
                 System.out.println("You can't buy nothing!");
                 continue;
             }
 
+            // Shirts
+            if (presetShirts == -1) {
+                System.out.println("How many of them are shirts");
+                amountShirts = getIntInput(totalItems);
+            } else {
+                amountShirts = presetShirts;
+            }
+
+            // Licensed stickers
+            System.out.println("How many of them are licensed stickers");
+            amountLicensedStickers = getIntInput(totalItems - amountShirts);
+
+            // Other stickers
+            amountStickers = totalItems - (amountShirts + amountLicensedStickers);
+
+            // PRICE CALCULATION
+
             // Stickers
             int stickerCost = 0;
             if (amountStickers > 0) {
+
                 // Check if using the birthday coupon
                 if (COUPON && amountStickers >= 4) {
 
                     int paidStickers = 0;
                     int freeStickers = 0;
-                    int amountTemp = amountStickers; //int amountTemp = amountStickers - amountLicensedStickers;
+                    int amountTemp = amountStickers + amountLicensedStickers;
 
+                    // Every 4th sticker is free, up to 5 times
                     while (freeStickers < 5 && amountTemp >= 4) {
                         amountTemp -= 4;
                         freeStickers += 1;
                         paidStickers += 3;
                     }
 
+                    // Licensed stickers are totaled seperately
                     paidStickers += amountTemp - amountLicensedStickers;
 
                     System.out.println("\nBirthday coupon:");
@@ -99,7 +107,7 @@ public class BSideLabel {
             int totalCost = stickerCost + shirtCost;
             int subtotal = totalCost;
 
-            System.out.println("Subtotal: " + totalCost + " yen, which is about " + (Math.round((double) totalCost / CONVERSION)) + " USD\n");
+            System.out.println("Subtotal: " + totalCost + " yen, which is about " + yenToUsd(totalCost) + " USD\n");
 
             // Donald trump fee
             if (TARIFF) {
@@ -114,9 +122,7 @@ public class BSideLabel {
                 System.out.println("Shipping: +" + SHIPPING + " yen");
             }
 
-            double usd = (double) totalCost / CONVERSION;
-
-            System.out.println("\nTOTAL COST:\n" + totalCost + " yen, which is about " + Math.round(usd) + " USD");
+            System.out.println("\nTOTAL COST:\n" + totalCost + " yen, which is about " + yenToUsd(totalCost) + " USD");
             System.out.println("You'll earn " + (subtotal / 200) + " points");
 
             System.out.println("\nPress enter to continue or type q to quit");
@@ -129,7 +135,39 @@ public class BSideLabel {
         sc.close();
     }
 
+    // Clears the console
     public static void clearConsole() {
         System.out.print("\033\143");
+    }
+
+    // Get an integer input between min and max
+    public static int getIntInput(int min, int max) {
+        while (true) {
+            try {
+                int input = Integer.parseInt(sc.nextLine());
+                if (input >= min && input <= max) {
+                    return input;
+                } else {
+                    System.out.println("Enter a valid integer between " + min + " and " + max);
+                }
+            } catch (Exception e) {
+                System.out.println("Enter a valid integer");
+            }
+        }
+    }
+
+    // Get an integer input greater than 0
+    public static int getIntInput() {
+        return getIntInput(0, Integer.MAX_VALUE);
+    }
+
+    // Get an integer input between 0 and max
+    public static int getIntInput(int max) {
+        return getIntInput(0, max);
+    }
+
+    // Converts yen to usd with the set conversion rate
+    public static int yenToUsd(int yen) {
+        return (int) Math.round((double) yen / CONVERSION);
     }
 }
